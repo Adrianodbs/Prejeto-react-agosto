c2efc9ac5cf961317138e3afc28bb655af1362af
Adriano <adriano_alves_2008@hotmail.com>
2022-08-18 09:21:13 -0300

    inicio depoimentos

---
+++ src/pages/home/Depoimentos/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Depoimentos() {
+  return (
+    <div className='depoimentos'>
+      <h3>Veja os depoimentos dos</h3>
+      <h2>Nossos clientes</h2>
+    </div>
+  )
+}
+
+export default Depoimentos
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,12 +3,14 @@ import './style.css'
 import Header from '../../components/header'
 import BemVindo from './bemvindo'
 import Encomenda from "./encomenda";
+import Depoimentos from './Depoimentos';
 
 function Home() {
   return <div>
     <Header />
     <BemVindo />
     <Encomenda />
+    <Depoimentos />
   </div>
 }
 
