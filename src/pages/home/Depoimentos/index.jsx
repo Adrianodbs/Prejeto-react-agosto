93113693616b6f4b46c77516ebca0d14375054de
Adriano <adriano_alves_2008@hotmail.com>
2022-08-19 13:22:50 -0300

    depoimentos clientes

--- src/pages/home/Depoimentos/index.jsx
+++ src/pages/home/Depoimentos/index.jsx
@@ -5,6 +5,14 @@ function Depoimentos() {
     <div className='depoimentos'>
       <h3>Veja os depoimentos dos</h3>
       <h2>Nossos clientes</h2>
+      <div className='depoimentos__content'>
+        <img src="./img/cristina.png" alt="" />
+        <div className="depoimentos__texto">
+          <h2>Cristina Alves</h2>
+          <h4>Naked de Brigadeiro</h4>
+          <p>Donec sit amet mi a lorem gravida sollicitudin eget nec nisl. Morbi et malesuada lectus. Donec ornare quis elit non bibendum. In vitae lacus velit. Ut ut turpis vitae nibh fringilla finibus a at massa. Ut quis bibendum nibh, tincidunt mattis diam. In ut ligula eu augue hendrerit laoreet id et ligula.</p>
+        </div>
+      </div>
     </div>
   )
 }
