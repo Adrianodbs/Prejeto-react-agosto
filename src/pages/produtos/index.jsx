54a8fd8612c37f3c58428ffab8b86d0d535d4a77
Adriano <adriano_alves_2008@hotmail.com>
2022-08-21 11:04:46 -0300

    finalizando produtos

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -3,8 +3,24 @@ import './style.css'
 
 function Produtos() {
   return (
-    <div>
+    <div className='produtos'>
       <MenuProdutos />
+      <div className="produtos__info">
+        <div className="produtos__info-texto">
+          <p>Faça sua </p>
+          <h2>Encomenda</h2>
+        </div>
+        <div className="produtos__info-redes">
+          <div className="produtos__info-redes--contato">
+            <img src="./img/logo_ig.png" alt="Instagram" />
+            <p>@villadosbolos</p>
+          </div>
+          <div className="produtos__info-redes--contato">
+            <img src="./img/logo_wp.png" alt="Whatsapp" />
+            <p>(71) 99123-4567</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
