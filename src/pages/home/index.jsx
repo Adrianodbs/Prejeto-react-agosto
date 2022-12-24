48c8e9ec3bc2a225eebdfea6ae5fc1ee652ba637
Adriano <adriano_alves_2008@hotmail.com>
2022-12-23 22:59:27 -0300

    correções css pt1

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,22 +2,21 @@ import React from 'react'
 import './style.css'
 import Header from '../../components/header'
 import BemVindo from './bemvindo'
-import Encomenda from "./encomenda";
-import Depoimentos from './Depoimentos';
-import Confeitaria from './confeitaria';
-import Contato from './contato';
-import Footer from '../../components/footer';
+import Encomenda from './encomenda'
+import Depoimentos from './Depoimentos'
+import Confeitaria from './confeitaria'
+import Contato from './contato'
 
 function Home() {
-  return <div>
-
-    <BemVindo />
-    <Encomenda />
-    <Confeitaria />
-    <Depoimentos />
-    <Contato />
-
-  </div>
+  return (
+    <div className="home">
+      <BemVindo />
+      <Encomenda />
+      <Confeitaria />
+      <Depoimentos />
+      <Contato />
+    </div>
+  )
 }
 
 export default Home
