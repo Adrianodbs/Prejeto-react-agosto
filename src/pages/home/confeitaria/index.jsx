7dabcfc103b533ef6845915caf03192174eb07af
Adriano <adriano_alves_2008@hotmail.com>
2023-03-15 20:10:04 -0300

    remoção do corrosel

--- src/pages/home/confeitaria/index.jsx
+++ src/pages/home/confeitaria/index.jsx
@@ -3,61 +3,25 @@ import './style.css'
 
 import { useState, useEffect, useRef } from 'react'
 
-import { motion } from 'framer-motion'
-
 function Confeitaria() {
-  const carousel = useRef()
-
-  const [width, setWidth] = useState(0)
-
-  useEffect(() => {
-    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
-  }, [])
   return (
     <div className="confeitaria">
       <h4>Conheça nossa</h4>
       <h2>Confeitaria</h2>
-      <motion.div
-        ref={carousel}
-        className="carousel"
-        whileTap={{ cursor: 'grabbing' }}
-      >
-        <motion.div
-          className="confeitaria__bolos"
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
-        >
-          <motion.div className="carousel__item">
-            <CardBolo
-              className="item"
-              imagem="./img/bolo_1.png"
-              titulo="Bolos"
-            />
-          </motion.div>
-          <motion.div className="carousel__item">
-            <CardBolo
-              className="item"
-              imagem="./img/bolo_2.png"
-              titulo="Tortas"
-            />
-          </motion.div>
-          <motion.div className="carousel__item">
-            <CardBolo
-              className="item"
-              imagem="./img/bolo_3.png"
-              titulo="Vulcões"
-            />
-          </motion.div>
-          <motion.div className="carousel__item">
-            {' '}
-            <CardBolo
-              className="item"
-              imagem="./img/bolo_4.png"
-              titulo="Bolo de Taça"
-            />
-          </motion.div>
-        </motion.div>
-      </motion.div>
+      <div className="confeitaria__produtos">
+        <CardBolo className="item" imagem="./img/bolo_1.png" titulo="Bolos" />
+        <CardBolo className="item" imagem="./img/bolo_2.png" titulo="Tortas" />
+        <CardBolo
+          className="item"
+          imagem="./img/bolo_3.png"
+          titulo="Vulcões"
+        />{' '}
+        <CardBolo
+          className="item"
+          imagem="./img/bolo_4.png"
+          titulo="Bolo de Taça"
+        />
+      </div>
     </div>
   )
 }
